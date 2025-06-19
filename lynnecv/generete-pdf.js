184973375db38a52c183f6138ccbda4317822f85
RoselineDC <roseline.danga@gmail.com>
2025-06-19 22:40:03 +0200

    new cv

--- lynnecv/generete-pdf.js
+++ lynnecv/generete-pdf.js
@@ -5,11 +5,11 @@ const path = require('path');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  const filePath = path.resolve(__dirname, 'index.html');
+  const filePath = path.resolve(__dirname, 'resume.html');
   await page.goto(`file://${filePath}`, { waitUntil: 'networkidle0' });
 
   await page.pdf({
-    path: 'Roseline Dangazela C-V.pdf',
+    path: 'Roseline Dangazela Resume.pdf',
     format: 'A4',
     printBackground: true,
   });
